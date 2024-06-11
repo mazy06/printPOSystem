63a92790cd6646b65a3c0d9a0bb8eee967904bee
Mazy Toufik <mazytoufik@outlook.com>
2024-06-11 23:46:18 +0200

    version 1 -> delay connection

--- src/main/java/io/mazy/printposystem/controller/HomeController.java
+++ src/main/java/io/mazy/printposystem/controller/HomeController.java
@@ -1,7 +1,10 @@
 package io.mazy.printposystem.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class HomeController {
@@ -9,4 +12,16 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> testConnection(@RequestParam String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            return ResponseEntity.ok("Connection successful: " + response.getBody());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Connection failed: " + e.getMessage());
+        }
+    }
 }
+
