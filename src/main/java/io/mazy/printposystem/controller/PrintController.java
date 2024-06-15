f136d4efeb6e11d4c20d1b30c6399d132468b76e
Mazy Toufik <mazytoufik@outlook.com>
2024-06-16 01:03:37 +0200

    version 2 : autoriser l'impression uniquement ceux qui sont connecté au reseau local

--- src/main/java/io/mazy/printposystem/controller/PrintController.java
+++ src/main/java/io/mazy/printposystem/controller/PrintController.java
@@ -1,12 +1,15 @@
 package io.mazy.printposystem.controller;
 
 import io.mazy.printposystem.service.PrinterService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.print.PrintException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Map;
 
 @RestController
@@ -19,16 +22,30 @@ public class PrintController {
         this.printService = printService;
     }
 
+    private static final String ALLOWED_DDNS_HOSTNAME = "printposystem.ddns.net";
+
     @PostMapping("/print")
-    public String printTicket(@RequestBody Map<String, String> request) throws Exception {
+    public String printTicket(@RequestBody Map<String, String> request, HttpServletRequest httpRequest) throws Exception {
         String ticketContent = request.get("content");
-        // Appeler le service d'impression
+
+        String clientIP = httpRequest.getRemoteAddr();
+
         try {
-            printService.print(ticketContent);
-            return "Ticket envoyé à l'imprimante";
-        } catch (PrintException e) {
-            return e.getMessage();
-            //return STR."Erreur lors de l'envoi du ticket : \{e.getMessage()}";
+            // Résoudre l'adresse IP du nom de domaine dynamique
+            InetAddress allowedIP = InetAddress.getByName(ALLOWED_DDNS_HOSTNAME);
+
+            if (clientIP.equals(allowedIP.getHostAddress())) {
+                try {
+                    printService.print(ticketContent);
+                    return "Ticket envoyé à l'imprimante";
+                } catch (PrintException e) {
+                    return "Erreur lors de l'envoi du ticket : " + e.getMessage();
+                }
+            } else {
+                return "Accès refusé";
+            }
+        } catch (UnknownHostException e) {
+            return "Erreur de résolution de l'adresse IP";
         }
     }
 }
